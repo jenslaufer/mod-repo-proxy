1b1aa1ea8f004818051876627da1ab019422fbff
Jens Laufer <jenslaufer@gmail.com>
2012-11-16 18:01:02 +0100

    fixed

--- mod-repo-proxy/src/main/groovy/Server.groovy
+++ mod-repo-proxy/src/main/groovy/Server.groovy
@@ -1,9 +1,9 @@
 import static org.vertx.groovy.core.streams.Pump.createPump
 import org.vertx.groovy.core.http.RouteMatcher
+import org.vertx.groovy.core.buffer.Buffer
 
 
-def client = vertx.createHttpClient()
-		.setHost("vert-x.github.com").setPort(80)
+def client = vertx.createHttpClient([host:"57.56.20.40",port: 3128])
 def filesRoot = System.getProperty("user.home")+"/.vertx/server"
 def rm = new RouteMatcher()
 
@@ -28,30 +28,35 @@ rm.put('/install/:path/:modId') { req ->
 	}
 }
 
-rm.get('/vertx-mods/mods/:path/:modId') { req ->
+rm.get('/vertx-mods/mods/:path/:modId') {
+	req ->
+	def path = "${filesRoot}/${req.params['path']}"
 	def fullPath = "${filesRoot}/${req.params['path']}/${req.params['modId']}"
 	if(vertx.fileSystem.existsSync(fullPath)){
 		req.response.sendFile fullPath
 	} else{
-		//req.response.end "Mod ${req.params['path']}/${req.params['modId']} not found"
-		def c_req = client.request(req.method, req.uri) { c_res ->
-			println "Proxying response: ${c_res.statusCode}"
-			req.response.chunked = true
-			req.response.statusCode = c_res.statusCode
-			req.response.headers << c_res.headers
-			c_res.dataHandler { data ->
-				println "Proxying response body: $data"
-				req.response << data
+		def clientRequest = client.request("GET", "http://vert-x.github.com/${req.uri}") { clientResponse ->
+			if(clientResponse.statusCode == 200){
+				def buffer = new Buffer()
+				clientResponse.dataHandler {
+					data -> buffer << data
+				}
+				clientResponse.endHandler{
+					vertx.fileSystem.mkdirSync(path, null, true)
+					vertx.fileSystem.open(fullPath) {ares ->
+						def file = ares.result
+						file.writeStream.writeBuffer(buffer)
+						file.close(){
+							println "closed"
+							req.response.sendFile fullPath
+						}
+					}
+				}
+			}else{
+				req.response.end "mod ${req.uri} not found"
 			}
-			c_res.endHandler { req.response.end() }
 		}
-		c_req.chunked = true
-		c_req.headers << req.headers
-		req.dataHandler { data ->
-			println "Proxying request body ${data}"
-			c_req << data
-		}
-		req.endHandler{ c_req.end() }
+		clientRequest.end()
 	}
 }
 
@@ -60,4 +65,4 @@ rm.noMatch{ req ->
 	req.response.end "${req.uri} not matched"
 }
 
-vertx.createHttpServer().requestHandler(rm.asClosure()).listen(9090,"localhost")
+vertx.createHttpServer().requestHandler(rm.asClosure()).listen(80,"localhost")
